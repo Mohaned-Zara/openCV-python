67f64a50d2f9f156e1d97174ab9a9f57faf8ecbb
Mohaned-Zara <mohanedzare@gmail.com>
2025-05-23 01:31:33 +0300

    added a code & resources

---
+++ 01-basics/1.1- Getting Started with Images.py
@@ -0,0 +1,18 @@
+"""
+# what I've learned:
+Read an image from file (using cv::imread)
+Display an image in an OpenCV window (using cv::imshow)
+Write an image to a file (using cv::imwrite)
+"""
+import cv2 as cv
+import sys
+ 
+img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+if img is None:
+    sys.exit("Could not read the image.")
+ 
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+ 
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
