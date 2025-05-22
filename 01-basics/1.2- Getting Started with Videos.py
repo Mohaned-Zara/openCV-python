58adb3fda23689aa8e67411e24af0f68f9f221c8
Mohaned-Zara <mohanedzare@gmail.com>
2025-05-23 01:50:33 +0300

    start1.2

---
+++ 01-basics/1.2- Getting Started with Videos.py
@@ -0,0 +1,7 @@
+"""
+# what I've learned:
+read video, display video, and save video.
+capture video from a camera and display it.
+these functions : cv.VideoCapture(), cv.VideoWriter()
+"""
+import cv2 as cv
