cb19393eebae3910e1798446ceb757084c69e429
Mohaned-Zara <mohanedzare@gmail.com>
2025-05-23 01:48:22 +0300

    done1.1

--- 01-basics/1.1- Getting Started with Images.py
+++ 01-basics/1.1- Getting Started with Images.py
@@ -5,14 +5,13 @@ Display an image in an OpenCV window (using cv::imshow)
 Write an image to a file (using cv::imwrite)
 """
 import cv2 as cv
-import sys
  
-img = cv.imread(cv.samples.findFile("starry_night.jpg"))
-if img is None:
-    sys.exit("Could not read the image.")
- 
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
- 
-if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+img_BGR = cv.imread("Resources\Photos\park.jpg",cv.IMREAD_COLOR) #MREAD_COLOR loads the image in the BGR 8-bit format. This is the default that is used here.
+img_original = cv.imread("Resources\Photos\park.jpg",cv.IMREAD_UNCHANGED) #IMREAD_UNCHANGED loads the image as is.
+img_Gray = cv.imread("Resources\Photos\park.jpg",cv.IMREAD_GRAYSCALE) #IMREAD_GRAYSCALE loads the image as an intensity one.
+
+cv.imshow("Display_BGR",img_BGR)
+cv.imshow("Display_image as is", img_original)
+cv.imshow("Display_GrayScale", img_Gray)
+
+cv.waitKey(0)
